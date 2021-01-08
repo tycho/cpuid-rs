00aa7fc8db7aeb86da5efe2c9745369d557e011e
Steven Noonan <steven@uplinklabs.net>
2021-01-07 21:06:20 -0800

    decode: cleanup
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>

--- src/bin/decode.rs
+++ src/bin/decode.rs
@@ -19,12 +19,6 @@ fn main() {
         "Parse and import dump file instead of reading from local CPUs",
         "FILE",
     );
-    opts.optopt(
-        "c",
-        "cpu",
-        "Which CPU to decode CPUID information from",
-        "INDEX",
-    );
     opts.optflag("h", "help", "Print this help text");
     let matches = match opts.parse(&args[1..]) {
         Ok(m) => m,
@@ -37,32 +31,6 @@ fn main() {
         return;
     }
 
-    // TODO: This kinda sucks because it will silently eat bogus values. We want
-    // it to eventually accept integer values (21), integer ranges (21-35),
-    // integer lists (21,22,23), or the string "all" (or similar).
-    let cpu_index: i32 = matches
-        .opt_str("cpu")
-        .unwrap_or("0".to_string())
-        .parse::<i32>()
-        .unwrap_or(0);
-
-    let mut cpu_start: u32 = 0;
-    let mut cpu_end: u32 = num_cpus::get() as u32 - 1;
-
-    if cpu_index > cpu_end as i32 {
-        panic!(
-            "CPU {} does not exist (valid range: {} to {})",
-            cpu_index, cpu_start, cpu_end
-        );
-    }
-
-    // For now we only accept a single CPU index in the --cpu argument, and set
-    // the range to only include that value.
-    if cpu_index >= 0 {
-        cpu_start = cpu_index as u32;
-        cpu_end = cpu_index as u32;
-    }
-
     env_logger::init();
 
     let system = match matches.opt_str("file") {
@@ -70,13 +38,6 @@ fn main() {
         _ => System::from_local(),
     };
 
-    for processor in system.cpus.iter() {
-        if processor.index < cpu_start || processor.index > cpu_end {
-            continue;
-        }
-        //println!("CPU {}:", processor.index);
-    }
-    //println!("{:#?}", system.caches());
     println!("{: >16}: {:?}", "Vendor(s)", system.vendor);
     println!("{: >16}: {}", "Processor Name", system.name_string);
     println!("\n{}", system.caches);
